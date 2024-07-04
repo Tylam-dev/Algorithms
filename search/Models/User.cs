5aa591385b40e236a6ce201f0aaaf8fa6d766e87
Tylam.dev <tylam.dev@gmail.com>
2024-07-04 17:35:16 -0500

    Adds Lineal Search

---
+++ search/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace search;
+
+public class User
+{
+    public int id {get;set;}
+    public string first_name {get;set;}
+    public string last_name {get;set;}
+    public string email_name {get;set;}
+    public string gender {get;set;}
+    public string ip_address {get;set;}
+}
---
+++ search/Searcher.cs
@@ -0,0 +1,15 @@
+﻿namespace search;
+
+public class Searcher
+{
+    public void LinealSearchByName(List<User> users, string name)
+    {
+        foreach (var item in users)
+        {
+            if (item.first_name == name){
+                Console.WriteLine($"User found \nid: {item.id}\nname: {item.first_name}");
+                break;
+            } 
+        }
+    }
+}
